835b88fb38c3cd28b845dcbc18f45662d459a0c0
ehdxorhrl <leedj6477@naver.com>
2023-10-19 23:23:29 +0900

    텀프로젝트 구성을 위한 기본 py파일들 생성

---
+++ Game_World.py
@@ -0,0 +1,14 @@
+from pico2d import *
+
+def handle_events():
+    global running
+
+    events = get_events()
+    for event in events:
+        if event.type == SDL_QUIT:
+            running = False
+        elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
+            running = False
+        else:
+            # boy.handle_event(event)
+
