a84ead933181982c6f7001330155994bcc334822
ehdxorhrl <leedj6477@naver.com>
2023-12-03 07:20:25 +0900

    스크롤링 허들 추가
    반대로 지나가도록 추가하였다

--- hurdle.py
+++ hurdle.py
@@ -6,15 +6,19 @@ class Hurdle:
         self.x, self.y = 400, 266
         self.state = 'stand'
 
+    def set_background(self, bg):
+        self.bg = bg
+
     def update(self):
+        self.sx = (self.x - self.bg.window_left)
         pass
 
     def draw(self):
         if self.state == 'stand':
-            self.image.draw(400, 200, 100, 100)
+            self.image.draw(self.sx, 200, 100, 100)
             draw_rectangle(*self.get_bb())
         elif self.state == 'lay_down':
-            self.image.rotate_draw(math.radians(270), 450, 200, 80, 100)
+            self.image.rotate_draw(math.radians(270), self.sx + 50, 200, 80, 100)
 
     def get_bb(self):
         if self.state == 'stand':
